34163faefde78931c3e8e956fb93f4a3c4766d29
SARANAJA <jacinto@iesvirgendelcarmen.com>
2016-10-09 11:30:38 +0200

    EJEMPLO DE VERSIONES

---
+++ app/src/main/java/es/vcarmen/versiones/Versiones.java
@@ -0,0 +1,14 @@
+package es.vcarmen.versiones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Versiones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_versiones);
+    }
+}
+    
